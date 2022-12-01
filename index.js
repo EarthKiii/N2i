fdb2073be1cf00fec8b353817997e6925c103963
ivan-bortnik <ivan.bortnik@protonmail.com>
2022-12-01 18:33:51 +0100

    serveur qui marche mais vraiment

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const path = require('path');
 
 const PORT = 3000;
 
+app.use('/public', express.static(path.join(__dirname, '/public')));
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/index.html'));
 });
